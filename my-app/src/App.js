70b1ee36ec12f7b67172cef6cd023cb611022156
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-03-09 18:41:12 -0500

    mvp is done and need to work on styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -20,6 +20,18 @@ class App extends React.Component{
        .catch((error) => {
          console.log(error)
        })
+
+    axios
+       .get("https://api.github.com/users/ShahJalpa/followers")
+       .then((response) => {
+         console.log(response)
+         this.setState({
+           followers: response.data
+         })
+       })
+       .catch((error) => {
+         console.log(error)
+       })
   }
   render(){
     return(
@@ -27,15 +39,26 @@ class App extends React.Component{
         <div className="title">
             <h1>Github User Card</h1>
         </div>
-        <div className="userDetail">
-            <img src={this.state.user.avatar_url} />
-            <h2>Name: {this.state.user.name}</h2>
-            <p>Location: {this.state.user.location}</p>
-            <p>Repositories: {this.state.user.public_repos}</p>
-            <p>Followers: {this.state.user.followers}</p>
-            <p>Following: {this.state.user.following}</p>
-            <p>Link: {this.state.user.html_url}</p>
-
+        <div className="userCard">
+            <div>
+                <img src={this.state.user.avatar_url} alt={this.state.user.login} />
+            </div>
+            <div className="userDetails">
+              <h2>{this.state.user.name}</h2>
+              <p>Location: {this.state.user.location}</p>
+              <p>Repositories: {this.state.user.public_repos}</p>
+              <p>Link: {this.state.user.html_url}</p>
+              <p>Followers: {this.state.user.followers}</p>
+              <p>Following: {this.state.user.following}</p>
+            </div>
+        </div>
+        <div className="followersCard">
+            {this.state.followers.map((follower) => 
+              <div>
+                <img src={follower.avatar_url} alt={follower.login} key={follower.id} />
+                <h2>{follower.login}</h2>
+              </div>
+            )}
         </div>
       </div>
     )
